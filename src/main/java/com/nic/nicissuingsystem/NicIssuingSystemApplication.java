a32cbd710bfae7a7216ae931f44c0d1b3163cb76
IT24103956 <IT24103956@my.sliit.lk>
2025-09-26 14:04:28 +0530

    Initial commit

---
+++ src/main/java/com/nic/nicissuingsystem/NicIssuingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nic.nicissuingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicIssuingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicIssuingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nic/nicissuingsystem/NicIssuingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nic.nicissuingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicIssuingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
