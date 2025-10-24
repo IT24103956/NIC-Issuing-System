8eac7f33bcd73fd551205c7c2bfe3668c4b141f8
IT24103956 <IT24103956@my.sliit.lk>
2025-10-24 13:59:54 +0530

    Add files via upload

---
+++ src/main/java/com/nic/nicissuingsystem/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nic.nicissuingplatform.repository;
+
+import com.nic.nicissuingplatform.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
